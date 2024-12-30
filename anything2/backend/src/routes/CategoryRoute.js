3304a4c5c0224875d6c45f330d12b83501ae8b39
Noura203040 <noura.salm@ejust.edu.eg>
2024-12-30 10:27:12 +0200

    Delete anything2 directory

--- anything2/backend/src/routes/CategoryRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCategory } = require('../controllers/CategoryController');
-
-// Define the route
-router.get('/categories/:category', getCategory);
-
-module.exports = router;
